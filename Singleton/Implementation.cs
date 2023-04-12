9ca823a96c9670c7c085f88f2bfbaae68a1ac1bb
Sevann Radhak <sevann.radhak@gmail.com>
2023-04-12 19:46:21 -0300

    Singleton Pattern

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,4 +1,23 @@
 ﻿namespace Singleton
 {
-  
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _lazyLogger = new(() => new Logger());
+        //private static Logger? _instance;
+
+        public static Logger? Instance => _lazyLogger.Value; //_instance ??= new Logger();
+
+        protected Logger()
+        {
+
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to Log: {message}");
+        }
+    }
 }
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Singleton";
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+Logger? instance1 = Logger.Instance;
+Logger? instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance1 == Logger.Instance)
+{
+    Console.WriteLine("Intances are the same.");
+}
+
+instance1?.Log($"Message from {nameof(instance1)}");
+instance2?.Log($"Message from {nameof(instance2)}");
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+Console.ReadLine();
